7609788c69dffd902a95a40ec9c8710bb1fcf9b8
Aniket Hendre <hendre.ani@gmail.com>
2024-05-19 18:48:26 +0530

    Move rag tests to test directory
    I had incorrectly added tests in same dir as source code.

--- test/git_agent/rag/core_test.clj
+++ test/git_agent/rag/core_test.clj
@@ -43,24 +43,24 @@
 (deftest rag-query-test
   (testing "Questions about codebase"
     (let [reply (rag/query "What is name of the project?")]
-      (is (re-matches #"(?i).*git[-_]?agent.*" reply)
+      (is (re-seq #"(?i).*git[-_]?agent.*" reply)
           "Name of the project should be returned as git-agent"))
     (let [reply (rag/query "Which programming languages are used in the codebase?")]
-      (is (re-matches #"(?i).*clojure.*" reply)
+      (is (re-seq #"(?i).*clojure.*" reply)
           "LLM should mention clojure programming language")))
 
   (testing "Ops related questions"
     (let [reply (rag/query "What is command for connecting using pgcli?")]
-      (is (re-matches #"(?i).*pgcli -h localhost -p 5432 -U git_agent -d gitagent.*" reply)
+      (is (re-seq #"(?i).*pgcli -h localhost -p 5432 -U git_agent -d gitagent.*" reply)
           "LLM should mention the pgcli command from readme file"))
     (let [reply (rag/query "Which build tool does this project use?")]
-      (is (re-matches #"(?i).*leiningen.*" reply)
+      (is (re-seq #"(?i).*leiningen.*" reply)
           "LLM should mention leiningen")))
 
   (testing "Questions about code contribution analysis"
     (let [reply (rag/query "Who has highest number of commits?")]
-      (is (re-matches #"(?i).*aniket hendre.*" reply)
+      (is (re-seq #"(?i).*aniket hendre.*" reply)
           "LLM should mention Aniket Hendre"))
     (let [reply (rag/query "When was the first commit added to repository?")]
-      (is (re-matches #"(?i).*may 10.*" reply)
+      (is (re-seq #"(?i).*may 10.*" reply)
           "LLM should mention the date of the first commit"))))
